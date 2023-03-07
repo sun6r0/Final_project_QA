2bfc78457727d95ce354db243f38a33b710375de
sun6r0 <55103268+sun6r0@users.noreply.github.com>
2023-03-07 22:46:36 +0400

    Update get_order_track_test.py

--- get_order_track_test.py
+++ get_order_track_test.py
@@ -5,7 +5,3 @@ import sender_stand_request
 def test_get_order_by_track():
     response = sender_stand_request.get_new_order_track()
     assert response.status_code == 200
-
-
-#    print(response.json()) можно вывести заказ в формате json
-#    print(response.text) можно вывести заказ в формате текста
